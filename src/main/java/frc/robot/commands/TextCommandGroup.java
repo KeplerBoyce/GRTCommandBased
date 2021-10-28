d6ce3ffbcde51799713e7351a5493ffb6d84b34c
KeplerBoyce <keplerboy@gmail.com>
2021-10-28 16:29:33 -0700

    moving stuff

---
+++ src/main/java/frc/robot/commands/TextCommandGroup.java
@@ -0,0 +1,70 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.stream.Stream;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.guzzler.GuzzlerIntakeCommand;
+import frc.robot.commands.swerve.DriveDistanceCommand;
+import frc.robot.commands.swerve.DriveTimeCommand;
+import frc.robot.commands.swerve.TurnCommand;
+import frc.robot.subsystems.GuzzlerSubsystem;
+import frc.robot.subsystems.SwerveSubsystem;
+
+public class TextCommandGroup extends SequentialCommandGroup {
+
+    public TextCommandGroup(SwerveSubsystem swerveSubsystem, GuzzlerSubsystem guzzlerSubsystem) {
+        //handle possible file not found and io exceptions
+        try {
+            //reading lines from text file
+            //path is the relative path to text file from GRTCommandBased folder
+            BufferedReader br = new BufferedReader(new FileReader("src\\main\\java\\frc\\robot\\commands\\testcommands.txt"));
+            Stream<String> lines = br.lines();
+
+            //go through lines one by one and add commands accordingly
+            lines.forEach(line -> {
+                //split lines into parts (separated in text file by spaces)
+                String[] parts = line.split(" ");
+                //for turn commands: parts = ["turn", direction, degrees]
+                //for drive commands: parts = ["drive", direction, mode(distance or time), meters/seconds]
+                //for intake commands: parts = ["intake"]
+
+                //check if this line is a turn or a drive command
+                switch (parts[0]) {
+                    case "turn":
+                        //add turn command (params explained above)
+                        addCommands(new TurnCommand(swerveSubsystem, parts[1], Float.parseFloat(parts[2])));
+                        break;
+                    case "drive":
+                        //check if driving by distance or by time
+                        if (parts[2].equals("distance")) {
+                            //add drive distance command (params explained above)
+                            addCommands(new DriveDistanceCommand(swerveSubsystem, parts[1], Float.parseFloat(parts[3])));
+                        } else if (parts[2].equals("time")) {
+                            //add drive time command (params explained above)
+                            addCommands(new DriveTimeCommand(swerveSubsystem, parts[1], Float.parseFloat(parts[3])));
+                        }
+                        break;
+                    case "intake":
+                        //add guzzler intake command
+                        addCommands(new GuzzlerIntakeCommand(guzzlerSubsystem));
+                        break;
+                }
+            });
+            //close buffered reader at the end
+            br.close();
+
+        } catch(FileNotFoundException e) {
+            System.out.println("File not found");
+        } catch(IOException e) {
+            System.out.println("Buffered reader is closed");
+        }
+    }
+}
